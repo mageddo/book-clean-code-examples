3e5a09708b02dca48697471fab22ef260a3631ed
Elvis de Freitas <edigitalb@gmail.com>
2024-05-09 17:11:34 -0300

    creating class

---
+++ src/main/java/cleancode/chapter02_meaningful_names/add_meaningful_context/listing_2_1/before/Main.java
@@ -0,0 +1,30 @@
+package cleancode.chapter02_meaningful_names.add_meaningful_context.listing_2_1.before;
+
+public class Main {
+  private void printGuessStatistics(char candidate, int count) {
+    String number;
+    String verb;
+    String pluralModifier;
+    if (count == 0) {
+      number = "no";
+      verb = "are";
+      pluralModifier = "s";
+    } else if (count == 1) {
+      number = "1";
+      verb = "is";
+      pluralModifier = "";
+    } else {
+      number = Integer.toString(count);
+      verb = "are";
+      pluralModifier = "s";
+    }
+    String guessMessage = String.format(
+      "There %s %s %s%s", verb, number, candidate, pluralModifier
+    );
+    print(guessMessage);
+  }
+
+  private void print(String msg) {
+    // the silence is golden
+  }
+}
